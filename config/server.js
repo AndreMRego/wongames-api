e3aaba043b3f2611d0d7f72b8513a2c390dd93f2
Andre Rego <andremregodev@gmail.com>
2020-12-17 14:05:21 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '115b72c324148235856b5642f56b6442'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5685436-36ee-4bd2-b697-8430911dd9e8'
+};
