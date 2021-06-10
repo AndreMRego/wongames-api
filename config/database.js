a819ca8888f6e4764aca43b5c7228d96292499e1
Andre Rego <andremregodev@gmail.com>
2021-06-10 12:18:35 -0300

    update config database

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
+        host: env('DATABASE_HOST', '192.168.171.61'),
+        port: env.int('DATABASE_PORT', 5433),
         database: env('DATABASE_NAME', 'wongames'),
         username: env('DATABASE_USERNAME', 'wongames'),
         password: env('DATABASE_PASSWORD', 'wongames123'),
