62428741fc6118295ce3eb82638a64b4c3e85328
Andre Rego <andremregodev@gmail.com>
2022-02-06 15:20:24 -0300

    Return 404 when there are no games found

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,12 +1,36 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  createPaymentIntent: () => {
-    return 'Hello World'
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all( cart?.map(async (game) => {
+      const validateGame = await strapi.services.game.findOne({
+        id: game.id,
+      });
+
+      if(validateGame) {
+        games.push(validateGame);
+      }
+    }))
+
+    if(!games.length) {
+      ctx.response.status = 404;
+
+      return {
+        error: 'No valid games found!'
+      }
+    }
+
+    return games;
   }
 };
