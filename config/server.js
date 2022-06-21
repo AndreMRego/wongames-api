d668ae213ac0d70a978d3a31a483c7ed9a523d3e
Andre Rego <idd_arego@uolinc.com>
2022-06-21 00:03:46 -0300

    Add domain url env

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('DOMAIN_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '115b72c324148235856b5642f56b6442'),
